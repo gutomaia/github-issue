59cf80d5a240d47d786cb3378897dc4d6fe556eb
Guto Maia <guto@guto.net>
2011-05-09 11:05:17 -0300

    changing from apache httpclient to java urlconnection

--- src/main/java/net/guto/jenkins/plugin/GitHubIssueHelper.java
+++ src/main/java/net/guto/jenkins/plugin/GitHubIssueHelper.java
@@ -1,46 +1,61 @@
 package net.guto.jenkins.plugin;
 
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.UsernamePasswordCredentials;
-import org.apache.commons.httpclient.auth.AuthScope;
-import org.apache.commons.httpclient.methods.PostMethod;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+
 
 public class GitHubIssueHelper {
 
 	// TODO: UGLY UGLY UGLY, please make it preatty
-	public void openIssue(Issue issue) {
-		HttpClient client = new HttpClient();
-		client.getState().setAuthenticationPreemptive(true);
-		UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("gutomaia", "ehehehehhe... :D");
-
-		client.getState().setCredentials(AuthScope.ANY, credentials);
-		PostMethod post = new PostMethod("https://api.github.com/repos/"
-				+ issue.user + "/" + issue.project + "/issues");
-		post.setRequestHeader("Content-Type", "application/json");
-		post.setRequestBody(issue.toJson());
-
-		try {
-			System.out.println(post.getURI());
-			int returnCode = client.executeMethod(post);
-						System.out.println(returnCode);
-			for(Header h :post.getRequestHeaders()){
-				System.out.println(h.getName() + ": " + h.getValue());
-			}
-			//System.out.println()
-			System.out.println(post.getResponseBodyAsString());
-		} catch (Exception e) {
+	public void openIssue(Issue issue) throws IOException {
+		String apiPage = "https://api.github.com/repos/"
+			+ issue.user + "/" + issue.project + "/issues";
+		String username = "gutomaia";
+		String password = "!!!!!";
+		
+		String authString = username+":"+password;
+		byte [] authEncoded = Base64.encodeBase64(authString.getBytes());
+				URL url = new URL(apiPage);
+		URLConnection con = url.openConnection();
+		
+		con.setRequestProperty("Authorization", "Basic " + new String(authEncoded));
+		con.setRequestProperty("Content-Type","application/json");
+		
+		//it's a post;
+		con.setDoOutput(true);
+		
+		OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+	    wr.write(issue.toJson());
+	    wr.flush();
+		
+		InputStream is = con.getInputStream();
+		InputStreamReader isr =  new InputStreamReader(is);
+		
+		int numCharsRead;
+		char[] charArray = new char[1024];
+		StringBuffer sb = new StringBuffer();
+		while ((numCharsRead = isr.read(charArray)) > 0) {
+			sb.append(charArray, 0, numCharsRead);
 		}
+		String result = sb.toString();
+		System.out.println("*** BEGIN ***");
+		System.out.println(result);
+		System.out.println("*** END ***");
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Issue issue = new Issue();
 		issue.user = "gUTOnET";
 		issue.project = "test-git-issue";
-		issue.title = "teste do plugin do jenkins";
-		issue.body = "teste do plugin do jenkins epa";
+		issue.title = "teste do plugin do jenkins 2";
+		issue.body = "teste do plugin do jenkins epa 2";
 		issue.assignee = "gUTOnET";
-		System.out.println(issue.toJson());
 		new GitHubIssueHelper().openIssue(issue);
 	}
 
